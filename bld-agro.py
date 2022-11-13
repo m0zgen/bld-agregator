fccca91b026d5701c6f982fef46cf83fe8bcf0a5
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-11-13 14:37:20 +0600

    + allowing lists

--- bld-agro.py
+++ bld-agro.py
@@ -4,7 +4,7 @@ ext_cfg = """
 """
 
 # unified = blocklist_aggregator.fetch(cfg_update=ext_cfg)
-unified = blocklist_aggregator.fetch(cfg_filename="config.cfg")
+unified = blocklist_aggregator.fetch(cfg_filename="blocking.yml")
 
 with open("whitelist.yml") as f:
     ext_cfg += "\n" + f.read()
@@ -16,3 +16,24 @@ blocklist_aggregator.save_raw(filename="blocklist.txt", cfg_update=ext_cfg)
 blocklist_aggregator.save_hosts(filename="hosts.txt", cfg_update=ext_cfg) 
 blocklist_aggregator.save_cdb(filename="blocklist.cdb", cfg_update=ext_cfg)
 
+allow_yaml = """
+verbose: true
+whitelist: []
+blacklist: []
+sources:
+  - urls:
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/whitelist.txt
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/vendors-wl/google.txt
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/vendors-wl/microsoft.txt
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/vendors-wl/yandex.txt
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/regex/common-wl.txt
+    pattern: ^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$
+
+  - urls:
+      - https://raw.githubusercontent.com/m0zgen/dns-hole/master/regex/common-wl.txt
+    pattern: ^(\/.*\/)$
+"""
+
+unified = blocklist_aggregator.fetch(cfg_update=allow_yaml)
+blocklist_aggregator.save_raw(filename="allowlist.txt", cfg_update=allow_yaml)
+blocklist_aggregator.save_cdb(filename="allowlist.cdb", cfg_update=allow_yaml)
